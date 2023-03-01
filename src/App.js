9501d1c411bd32a710ccd70f965905bf0637a29b
Virul Nirmala <virulnirmala24@gmail.com>
2023-03-01 16:34:46 +0530

    new

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <AnimatePresence >
       <Routes key={location.pathname} location={location}>
-        <Route path="" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/More" element={<More />} />
         <Route path="/About" element={<About />} />
       </Routes>
